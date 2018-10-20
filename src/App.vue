0143ff32c76f90b915946dd0b855d7ce33cd6d45
unknown <piant.grunger@gmail.com>
2018-10-20 13:57:52 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id='app'>
+    <h1>{{title}}</h1>
+    <Manga        />
+    </div>
+    </div>
+</template>
+<script>
+    import Manga  from './component/Manga.vue'
+    export default {
+        name: 'app',
+        data() {
+            return {
+                title: 'Manga List',
+           
+            }
+        },
+        methods: {
+          
+        },
+        components:{
+            Manga
+        }
+    }
+</script>
---
+++ src/component/DetailManga.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="row">
+<div class="col-md-2">
+<img :src="imageManga" class="img-fluid img-thumbnail">
+</div>
+<div class="col-md-2">
+<h3>{{data.t}}</h3>
+<p>alias : {{data.a}}</p>
+
+<p>category : {{data.c}}</p>
+
+
+</div>
+</div>
+</template>
+<script>
+    export default{
+        name: "DetailManga",
+        props :["data"],
+       data(){
+           return {
+               imageManga :  'http://cdn.mangaeden.com/mangasimg/'+this.data.im,
+            
+            
+          
+           }
+       },
+       
+methods: {
+  async getLink(){
+    let response = await PostsService.downloadURL({
+      imgURL : this.imageManga
+    })
+    console.log(response.data)
+    this.link = response.data
+  }
+},
+created() {
+  // when the instance ist create call the method
+  this.getLink();
+},
+       
+
+    }
+</script>
